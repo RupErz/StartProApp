553f36fbab01f2d5a1d54af9affbd614a4117b9e
Nghia <nghiavu144@gmail.com>
2024-11-07 21:07:38 -0600

    Finishing user profile with proper skeleton , PPR , and modify navbar with new shadcn avatar

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {formatDate} from "@/lib/utils"
+import {cn, formatDate} from "@/lib/utils"
 import { EyeIcon } from 'lucide-react'
 import Link from "next/link"
 import Image from "next/image";
 import { Button } from './ui/button';
 import { Startup, Author } from "@/sanity/types"
+import { Skeleton } from './ui/skeleton';
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -43,7 +44,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                     </Link>
                 </div>
                 <Link href={`/user/${author?._id}`}>
-                    <Image src= "https://placehold.co/48x48" alt="placeholder" width= {48} height= {48} className= "rounded-full"/>
+                    <Image src= {author?.image!} alt="placeholder" width= {48} height= {48} className= "rounded-full"/>
                 </Link>
             </div>
 
@@ -67,5 +68,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li>
     )
 }
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+            <li key={cn('skeleton', index)}>
+                <Skeleton className='startup-card_skeleton' />
+            </li>
+        ))}
+    </>
+);
 
 export default StartupCard
